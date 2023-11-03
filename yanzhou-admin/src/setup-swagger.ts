27d615b616b1b2e23e6a792758133108916cba54
wangyang <724319481@qq.com>
2023-11-03 16:19:46 +0800

    文档

--- yanzhou-admin/src/setup-swagger.ts
+++ yanzhou-admin/src/setup-swagger.ts
@@ -1,14 +1,17 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+let fs = require('fs');
+
 export function setupSwagger(app: INestApplication): void {
   const swaggerConfig = new DocumentBuilder()
     .setTitle('砚舟')
     .setDescription('Api文档')
-    .setTermsOfService('https://docs.nestjs.cn/8/introduction')
     .setLicense('MIT', 'https://www.baidu.com')
-    .addBearerAuth()
+    // .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup(`swagger-ui`, app, document);
+
+  fs.writeFileSync('src/public/swagger-spec.json', JSON.stringify(document));
 }
